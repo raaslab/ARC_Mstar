982baa0f312eb4e87245bf50864c0707b0197d13
Prajwal Shanthakumar <prajwal.vt.edu>
2019-05-13 23:51:46 -0400

    online

--- Online_MinimaxMstar2.m
+++ Online_MinimaxMstar2.m
@@ -21,12 +21,12 @@ X_MAX = environment_max_x+0.1*(environment_max_x-environment_min_x);
 Y_MIN = environment_min_y-0.1*(environment_max_y-environment_min_y);
 Y_MAX = environment_max_y+0.1*(environment_max_y-environment_min_y);
 
-Initial_Robot = [8;11];
+Initial_Robot = [8;8];
 % Initial_Target = [7;9];
 % sensor_x = [6	6	6	6	6	6	6	6	6	6	6	6	6	7	8	8	8	8	8	7];
 % sensor_y =	[1	2	3	4	5	6	7	8	9	10	11	12	13	13	13	14	15	16	17	17];
-sensor_x =  [2];
-sensor_y =	[12];
+sensor_x =  [7];
+sensor_y =	[9];
 Teammate = [10,10];
 Record_Robot_path_x = Initial_Robot(1);
 Record_Robot_path_y = Initial_Robot(2);
@@ -58,9 +58,9 @@ for oo = 1:50
     format long;
     
     Negtive_Reward = 50.0;
-    Negtive_Teammate = 100.0;
+    Negtive_Teammate = 1000.0;
     Total_scan = false(1000,1000);
-    Vis = graph([1],[]);
+    Vis = digraph([1],[]);
     Vis.Nodes.Robot_x= Record_Robot_path_x(oo);
     Vis.Nodes.Robot_y= Record_Robot_path_y(oo);
     Vis.Nodes.Target_x= Record_Target_path_x(oo);
@@ -76,19 +76,20 @@ for oo = 1:50
     Vis.Nodes.Decision_Value = 0;
     Vis.Nodes.Decision_Node = 0;
     Vis.Nodes.Teammate_detected = 0;
+    Vis.Nodes.path = 0;
     Vis.Nodes.Prune = 0;
-    Vis.Nodes.QMAX = -10000;
-    Vis.Nodes.QMIN = 10000;
+    Vis.Nodes.QMAX = -1110;
+    Vis.Nodes.QMIN = -1000;
     
     level = 4;
     Count = 1;
     for l = 1:level
         if l == 1
-           start = 1;
-           en = 1;
+            start = 1;
+            en = 1;
         else
-           start = (4^(l-1)-1)/3 +1;
-           en = (4^(l)-1)/3;
+            start = (4^(l-1)-1)/3 +1;
+            en = (4^(l)-1)/3;
         end
         for j = start:en
             if mod(l,2)
@@ -97,7 +98,8 @@ for oo = 1:50
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 Vis.Nodes.Parent(Count+1) = j;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Robot_x(j)+1, Vis.Nodes.Target_y(j)] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Robot_x(j)+1, Vis.Nodes.Target_y(j)] , environment , epsilon )...
+                            &&in_environment( [Vis.Nodes.Robot_x(j)+0.5, Vis.Nodes.Target_y(j)] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j)+1;
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j);
@@ -108,6 +110,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = poly2mask(V{1}(:,1),V{1}(:,2),50, 50) | Vis.Nodes.Robot_Region{j};
                         Vis.Nodes.Robot_Reward(Count+1) = bwarea(Vis.Nodes.Robot_Region{Count+1});
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
+                        Vis.Nodes.QMAX(Count+1) = -1110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                     else
                         Vis.Nodes.Prune(Count+1) = -1;
                     end
@@ -121,7 +125,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Robot_x(j)-1, Vis.Nodes.Target_y(j)] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Robot_x(j)-1, Vis.Nodes.Target_y(j)] , environment , epsilon )...
+                            &&in_environment( [Vis.Nodes.Robot_x(j)-0.5, Vis.Nodes.Target_y(j)] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j)-1;
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j);
@@ -132,6 +137,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = poly2mask(V{1}(:,1),V{1}(:,2),50, 50) | Vis.Nodes.Robot_Region{j};
                         Vis.Nodes.Robot_Reward(Count+1) = bwarea(Vis.Nodes.Robot_Region{Count+1});
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                     else
                         Vis.Nodes.Prune(Count+1) = -1;
                     end
@@ -145,7 +152,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Robot_x(j), Vis.Nodes.Target_y(j)+1] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Robot_x(j), Vis.Nodes.Target_y(j)+1] , environment , epsilon )...
+                            && in_environment( [Vis.Nodes.Robot_x(j), Vis.Nodes.Target_y(j)+0.5] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j);
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j)+1;
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j);
@@ -156,6 +164,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = poly2mask(V{1}(:,1),V{1}(:,2),50, 50) | Vis.Nodes.Robot_Region{j};
                         Vis.Nodes.Robot_Reward(Count+1) = bwarea(Vis.Nodes.Robot_Region{Count+1});
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                     else
                         Vis.Nodes.Prune(Count+1) = -1;
                     end
@@ -180,6 +190,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = poly2mask(V{1}(:,1),V{1}(:,2),50, 50) | Vis.Nodes.Robot_Region{j};
                         Vis.Nodes.Robot_Reward(Count+1) = bwarea(Vis.Nodes.Robot_Region{Count+1});
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                     else
                         Vis.Nodes.Prune(Count+1) = -1;
                     end
@@ -195,7 +207,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Target_x(j)+1, Vis.Nodes.Target_y(j)] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Target_x(j)+1, Vis.Nodes.Target_y(j)] , environment , epsilon )...
+                            &&in_environment( [Vis.Nodes.Target_x(j)+0.5, Vis.Nodes.Target_y(j)] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j);
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j)+1;
@@ -205,7 +218,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = Vis.Nodes.Robot_Region{j} ;
                         Vis.Nodes.Robot_Reward(Count+1) = Vis.Nodes.Robot_Reward(j);
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
-                        
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                         
                         
                         if in_environment( [Vis.Nodes.Robot_x(Count+1) Vis.Nodes.Robot_y(Count+1)] , V , epsilon )
@@ -235,7 +249,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Target_x(j)-1, Vis.Nodes.Target_y(j)] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Target_x(j)-1, Vis.Nodes.Target_y(j)] , environment , epsilon )...
+                            && in_environment( [Vis.Nodes.Target_x(j)-0.5, Vis.Nodes.Target_y(j)] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j);
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j)-1;
@@ -245,7 +260,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = Vis.Nodes.Robot_Region{j} ;
                         Vis.Nodes.Robot_Reward(Count+1) = Vis.Nodes.Robot_Reward(j);
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
-                        
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                         if in_environment( [Vis.Nodes.Robot_x(Count+1) Vis.Nodes.Robot_y(Count+1)] , V , epsilon )
                             Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j)+1;
                         end
@@ -262,7 +278,7 @@ for oo = 1:50
                     Vis.Nodes.Prune(Count+1) = -1;
                 end
                 
-                if Vis.Nodes.Generation(Count+1) == level + 1 
+                if Vis.Nodes.Generation(Count+1) == level + 1
                     Vis.Nodes.QMAX(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward;
                     Vis.Nodes.QMIN(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward - Vis.Nodes.Teammate_detected(Count+1)*Negtive_Teammate;
                 end
@@ -273,7 +289,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)+1] , environment , epsilon )
+                   if in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)+1] , environment , epsilon )...
+                            && in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)+0.5] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j);
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j);
@@ -283,7 +300,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = Vis.Nodes.Robot_Region{j} ;
                         Vis.Nodes.Robot_Reward(Count+1) = Vis.Nodes.Robot_Reward(j);
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
-                        
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                         if in_environment( [Vis.Nodes.Robot_x(Count+1) Vis.Nodes.Robot_y(Count+1)] , V , epsilon )
                             Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j)+1;
                         end
@@ -300,8 +318,8 @@ for oo = 1:50
                     Vis.Nodes.Prune(Count+1) = -1;
                 end
                 if Vis.Nodes.Generation(Count+1) == level + 1
-                    Vis.Nodes.QMAX(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward;
-                    Vis.Nodes.QMIN(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward - Vis.Nodes.Teammate_detected(Count+1)*Negtive_Teammate;
+                    Vis.Nodes.QMAX(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward - Vis.Nodes.Teammate_detected(Count+1)*Negtive_Teammate;
+                    Vis.Nodes.QMIN(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward ;
                 end
                 Count = Count+1;
                 %%
@@ -309,7 +327,8 @@ for oo = 1:50
                 Vis.Nodes.Parent(Count+1) =j;
                 Vis.Nodes.Generation(Count+1) = Vis.Nodes.Generation(j)+1;
                 if Vis.Nodes.Prune(j) ~= -1
-                    if in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)-1] , environment , epsilon )
+                    if in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)-1] , environment , epsilon )...
+                            && in_environment( [Vis.Nodes.Target_x(j), Vis.Nodes.Target_y(j)-0.5] , environment , epsilon )
                         Vis.Nodes.Robot_x(Count+1) = Vis.Nodes.Robot_x(j);
                         Vis.Nodes.Robot_y(Count+1) = Vis.Nodes.Robot_y(j);
                         Vis.Nodes.Target_x(Count+1) = Vis.Nodes.Target_x(j);
@@ -319,7 +338,8 @@ for oo = 1:50
                         Vis.Nodes.Robot_Region{Count+1} = Vis.Nodes.Robot_Region{j} ;
                         Vis.Nodes.Robot_Reward(Count+1) = Vis.Nodes.Robot_Reward(j);
                         Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j);
-                        
+                        Vis.Nodes.QMAX(Count+1) = -110;
+                        Vis.Nodes.QMIN(Count+1) = -1000;
                         if in_environment( [Vis.Nodes.Robot_x(Count+1) Vis.Nodes.Robot_y(Count+1)] , V , epsilon )
                             Vis.Nodes.Detection_time(Count+1) = Vis.Nodes.Detection_time(j)+1;
                         end
@@ -336,8 +356,8 @@ for oo = 1:50
                     Vis.Nodes.Prune(Count+1) = -1;
                 end
                 if Vis.Nodes.Generation(Count+1) == level + 1
-                    Vis.Nodes.QMAX(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward;
-                    Vis.Nodes.QMIN(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward - Vis.Nodes.Teammate_detected(Count+1)*Negtive_Teammate;
+                    Vis.Nodes.QMAX(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward - Vis.Nodes.Teammate_detected(Count+1)*Negtive_Teammate;
+                    Vis.Nodes.QMIN(Count+1) = Vis.Nodes.Robot_Reward(Count+1) - Vis.Nodes.Detection_time(Count+1)*Negative_reward;
                 end
                 Count = Count+1;
                 %%
@@ -348,6 +368,43 @@ for oo = 1:50
             
         end
     end
+    
+    %find the optimal path
+    
+    T = nnz(Vis.Nodes.Generation);
+    Node_path = 1;
+    Best_node = 1;
+    for i = T:-1:1
+        if Vis.Nodes.Generation(i) >= level + 1
+            continue
+        else
+            sucIDs = successors(Vis,i);
+            for k = 1:nnz(sucIDs)
+                if mod(Vis.Nodes.Generation(i),2)&& Vis.Nodes.Prune(sucIDs(k)) ~= -1
+                    if Vis.Nodes.QMAX(i) < Vis.Nodes.QMAX(sucIDs(k))
+                        Vis.Nodes.QMAX(i) = min(Vis.Nodes.QMAX(sucIDs(k)),Vis.Nodes.QMAX(i));
+                        Vis.Nodes.QMIN(i) = max(Vis.Nodes.QMIN(sucIDs(k)),Vis.Nodes.QMIN(i));
+                        Vis.Nodes.path(i) = sucIDs(k);
+                    end
+                else
+                    if Vis.Nodes.QMIN(i) >= Vis.Nodes.QMIN(sucIDs(k)) && Vis.Nodes.Prune(sucIDs(k)) ~= -1
+                        Vis.Nodes.QMIN(i) = Vis.Nodes.QMIN(sucIDs(k));
+                        Vis.Nodes.QMAX(i) = Vis.Nodes.QMAX(sucIDs(k));
+                        Vis.Nodes.path(i) = sucIDs(k);
+                    end
+                end
+            end
+            
+        end
+    end
+    Robot_next = Vis.Nodes.path(1);
+    Target_next = Vis.Nodes.path(Robot_next);
+    
+    Record_Robot_path_x(oo+1) = Vis.Nodes.Robot_x(Robot_next);
+    Record_Robot_path_y(oo+1) = Vis.Nodes.Robot_y(Robot_next);
+    Record_Target_path_x(oo+1) = Vis.Nodes.Target_x(Target_next);
+    Record_Target_path_y(oo+1) = Vis.Nodes.Target_y(Target_next);
+    plot_environment(Record_Robot_path_x(oo),Record_Robot_path_y(oo),Record_Target_path_x(oo),Record_Target_path_y(oo))
     
     
     a = 1;
---
+++ plot_environment.m
@@ -0,0 +1,198 @@
+%
+%=========VisiLibity Demonstration Script=========
+%
+%This script uses the the MEX-files generated from
+%visibility_polygon.cpp and in_environment.cpp.  Follow
+%the instructions in the respective .cpp files to create
+%these MEX-files before running this script. A graphical
+%representation of the supplied environment file
+%example1.environment is dislplayed and the user can
+%then select points (must be in the environment) with
+%the mouse and the visibility polygon of that point will
+%be computed and plotted over the environment.
+%
+
+
+%Clear the desk
+
+
+function []= plot_environment(Robot_path_x, Robot_path_y, Target_path_x,Target_path_y)
+format long;
+
+%Robustness constant
+epsilon = 0.000000001;
+
+
+%Snap distance (distance within which an observer location will be snapped to the
+%boundary before the visibility polygon is computed)
+snap_distance = 0.05;
+
+
+%Read environment geometry from file
+environment = read_vertices_from_file('./MCTS2.environment');
+% sensor_x = [2,3,4,5,6,6,6,6,7,8,9,10,11,11,11,11,11,11];
+% sensor_y =	[3,3,3,3,3,4,5,6,6,6,6,6,6,7,8,9,10,11];
+
+
+% environment_sensor = environment_sensor(sensor_x,sensor_y,environment);
+sensor_detect_indicator = [0 0 0 0];
+Negative_reward = 50;
+
+%Calculate a good plot window (bounding box) based on outer polygon of environment
+environment_min_x = min(environment{1}(:,1));
+environment_max_x = max(environment{1}(:,1));
+environment_min_y = min(environment{1}(:,2));
+environment_max_y = max(environment{1}(:,2));
+X_MIN = environment_min_x-0.1*(environment_max_x-environment_min_x);
+X_MAX = environment_max_x+0.1*(environment_max_x-environment_min_x);
+Y_MIN = environment_min_y-0.1*(environment_max_y-environment_min_y);
+Y_MAX = environment_max_y+0.1*(environment_max_y-environment_min_y);
+
+
+
+%Clear plot and form window with desired properties
+clf; hold on;
+axis equal; axis off; axis([X_MIN X_MAX Y_MIN Y_MAX]);
+
+
+%Plot Environment
+patch( environment{1}(:,1) , environment{1}(:,2) , 0.1*ones(1,length(environment{1}(:,1)) ) , ...
+    'w' , 'linewidth' , 1.5 );
+for i = 2 : size(environment,2)
+    patch( environment{i}(:,1) , environment{i}(:,2) , 0.1*ones(1,length(environment{i}(:,1)) ) , ...
+        'k' , 'EdgeColor' , [0 0 0] , 'FaceColor' , [0.8 0.8 0.8] , 'linewidth' , 1.5 );
+end
+
+
+
+
+%Select test points and plot resulting visibility polygon
+
+% current_x =[2,3,4,3,3,2,1,1,2,2,3,4,5,5,6,7,7,7]; %
+% current_y =[14,14,14,14,13,13,13,12,12,13,13,13,13,14,14,14,15,16];
+
+% current_x = [7 7 7 7 8 9 10 ]; %
+% current_y = [8 7 6 5 5 5 5 ];
+%
+% sensor_x = [3  4  5  6  7  7  7];
+% sensor_y = [12 12 12 12 12 11 10];
+%
+% Teamate_x = [10	10 10 10 10	10 10];
+% Teamate_y = [10	10 10 10 10	10 10];
+
+current_x = Robot_path_x; %
+current_y = Robot_path_y;
+
+sensor_x = Target_path_x;
+sensor_y = Target_path_y;
+
+Teamate_x = [10];
+Teamate_y = [10];
+
+Total_scan = false(1000,1000);
+reward_step = 0;
+
+
+
+%Acquire test point.
+%[observer_x observer_y] = ginput(1);
+observer_x = current_x(1);
+observer_y = current_y(1);
+%Make sure the current point is in the environment
+if  in_environment( [observer_x observer_y] , environment , epsilon )
+    
+    %             Clear plot and form window with desired properties
+    clf;  hold on;
+    axis equal; axis off; axis([X_MIN X_MAX Y_MIN Y_MAX]);
+    
+    %Plot environment
+    patch( environment{1}(:,1) , environment{1}(:,2) , 0.1*ones(1,length(environment{1}(:,1)) ) , ...
+        'w' , 'linewidth' , 1.5 );
+    for i = 2 : size(environment,2)
+        patch( environment{i}(:,1) , environment{i}(:,2) , 0.1*ones(1,length(environment{i}(:,1)) ) , ...
+            'k' , 'EdgeColor' , [0 0 0] , 'FaceColor' , [0.8 0.8 0.8] , 'linewidth' , 0.1 );
+    end
+    
+    
+    
+    %             Plot observer
+    plot3( observer_x , observer_y , 0.3 , ...
+        'o' , 'Markersize' , 15 , 'MarkerEdgeColor' , 'k' , 'MarkerFaceColor' , 'r' );
+    hold on
+    
+    
+    W{1} = visibility_polygon( [sensor_x(1) sensor_y(1)] , environment , epsilon , snap_distance );
+    V{1} = visibility_polygon( [observer_x observer_y] , environment , epsilon , snap_distance );
+    
+    
+    %sensor polygon
+    
+    Area_sensor = polyarea(W{1}(:,1),W{1}(:,2));
+    patch( W{1}(:,1) , W{1}(:,2) , 0.1*ones( size(W{1},1) , 1 ) , ...
+        [0.7,0.7,0.9] , 'LineStyle' , 'none' );
+    %         plot3( W{1}(:,1) , W{1}(:,2) , 0.1*ones( size(W{1},1) , 1 ) , ...
+    %             'y*' , 'Markersize' , 5 );
+    plot3( sensor_x(1) , sensor_y(1) , 0.3 , ...
+        's' , 'Markersize' , 15, 'MarkerFaceColor' , [0.9,0.8,0.7],'MarkerFaceColor','b','MarkerEdgeColor','b' );
+    
+    plot3(Teamate_x,Teamate_y, 0.3 , ...
+        'p' , 'Markersize' , 20, 'MarkerFaceColor' , [0.9,0.8,0.7],'MarkerFaceColor','r','MarkerEdgeColor','r' );
+    %total polygon
+    
+    Total_visiable{1} =  V;
+    
+    for k = 1:1-1
+        tpatch = patch( Total_visiable{k}{1}(:,1) , Total_visiable{k}{1}(:,2) , 0.1*ones( size(Total_visiable{k}{1},1) , 1 ) , ...
+            [0.9,0.8,0.8] , 'LineStyle' , 'none' );
+        alpha(tpatch,0.6)
+    end
+    
+    %Compute and plot visibility polygon
+    
+    Area = polyarea(V{1}(:,1),V{1}(:,2));
+    
+    vpatch= patch( V{1}(:,1) , V{1}(:,2) , 0.1*ones( size(V{1},1) , 1 ) , ...
+        [0.9,0.5,0.5],'LineStyle' , 'none' );
+    %         plot3( V{1}(:,1) , V{1}(:,2) , 0.1*ones( size(V{1},1) , 1 ) , ...
+    %             'b*' , 'Markersize' , 5 );
+    alpha(vpatch, 0.6)
+    
+    
+    hold on
+    
+    
+    
+    %%overlap area
+    x1= V{1}(:,1);
+    y1= V{1}(:,2);
+    b1 = poly2mask(x1,y1,1000, 1000);
+    areaImage = bwarea(b1);
+    Total_scan = b1 | Total_scan;
+    reward_step(1) = bwarea(Total_scan);
+    
+    
+    
+    
+    plot3(sensor_x(1:1),sensor_y(1:1),0.1*ones( nnz(sensor_x(1:1)) , 1 ),'b','LineWidth',5)
+    plot3(current_x(1:1),current_y(1:1),0.1*ones( nnz(current_x(1:1)) , 1 ),'r','LineWidth',5)
+    pause(0.1)
+    hold off
+    
+    %      mov(1) = getframe(gca);
+    %      jj = 1;
+    %      imwrite(mov(1),sprintf('High%d.jpg',jj))
+    
+    %sensor the next point
+    
+    
+    
+end
+%
+% movie2avi(mov, 'equilibrium.avi', 'compression','None', 'fps',10);
+% winopen('equilibrium.avi')
+% hold on
+%  plot3(sensor_x,sensor_y,0.1*ones( nnz(sensor_x) , 1 ),'b','LineWidth',5)
+%  plot3(current_x,current_y,0.1*ones( nnz(current_x) , 1 ),'r','LineWidth',5)
+
+
+end
